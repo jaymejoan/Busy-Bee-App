6b13b13001576a326cdfff213ba8dcfaf3c00019
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-08-20 16:33:24 +1200

    Format FAB on screen

--- busy_bee/app/components/FloatingActionButton.js
+++ busy_bee/app/components/FloatingActionButton.js
@@ -3,7 +3,7 @@ import {
   View,
   StyleSheet,
   Animated,
-  TouchableWihtoutFeedback,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -14,18 +14,29 @@ import colors from "./colors";
 export default class FloatingActionButton extends React.Component {
   render() {
     return (
-      <AntDesign
-        name="pluscircleo"
-        size={70}
-        color={colors.black}
-        onPress={() => console.log("clicked FAB")}  // bring to new task page
-      />
+      <View style={[styles.container, this.props.style]}>
+        <TouchableWithoutFeedback>
+          <Animated.View styles={[styles.fab, styles.highlight]}>
+            <AntDesign
+              name="pluscircleo"
+              size={70}
+              color={colors.black}
+              onPress={() => console.log("clicked FAB")} // bring to new task page
+            />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    flex: 1,
+  },
+  fab: {
     width: 60,
     height: 60,
     borderRadius: 30,
@@ -35,5 +46,11 @@ const styles = StyleSheet.create({
     shadowColor: colors.yellow,
     shadowOpacity: 0.3,
     shadowOffset: { height: 10 },
+  },
+  highlight: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: colors.yellow,
   },
 });
