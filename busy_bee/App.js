8c4041432dad8533764b7eeecdc0effeb230670b
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-08-20 17:09:53 +1200

    Implement splash screen functionality

--- busy_bee/App.js
+++ busy_bee/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './app/screens/HomeScreen';
 import SplashScreen from './app/screens/SplashScreen';
+import * as SplashScreenTiming from 'expo-splash-screen';
+
+SplashScreenTiming.preventAutoHideAsync();
+setTimeout(SplashScreenTiming.hideAsync, 3000);
 
 export default function App() {
   return (
