57daf49d0913bddbafeab214ad5612571084db40
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-08-20 18:14:19 +1200

    Remove redundant code

--- busy_bee/app/components/FloatingActionButton.js
+++ busy_bee/app/components/FloatingActionButton.js
@@ -1,10 +1,4 @@
 import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-  Animated,
-  TouchableWithoutFeedback,
-} from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 import colors from "../config/colors";
@@ -14,45 +8,14 @@ import colors from "../config/colors";
 class FloatingActionButton extends Component {
   render() {
     return (
-      <View style={[styles.container, this.props.style]}>
-        <TouchableWithoutFeedback>
-          <Animated.View styles={[styles.fab, styles.highlight]}>
-            <AntDesign
-              name="pluscircleo"
-              size={70}
-              color={colors.black}
-              onPress={() => console.log("clicked FAB")} // bring to new task page
-            />
-          </Animated.View>
-        </TouchableWithoutFeedback>
-      </View>
+      <AntDesign
+        name="pluscircleo"
+        size={70}
+        color={colors.black}
+        onPress={() => console.log("clicked FAB")} // bring to new task page
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "flex-end",
-    flex: 1,
-  },
-  fab: {
-    width: 70,
-    height: 70,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    shadowRadius: 10,
-    shadowColor: colors.yellow,
-    shadowOpacity: 0.3,
-    shadowOffset: { height: 10 },
-  },
-  highlight: {
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    backgroundColor: colors.yellow,
-  },
-});
 
 export default FloatingActionButton;
