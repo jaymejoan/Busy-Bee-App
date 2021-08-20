5193e5f36eda0446dbc95d5c3bba2ac64d04f936
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-08-20 18:36:34 +1200

    Create NewTaskScreen

---
+++ busy_bee/app/screens/NewTaskScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Image, View, Button } from "react-native";
+
+
+const NewTaskScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default NewTaskScreen;
