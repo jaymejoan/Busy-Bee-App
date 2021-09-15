3d1f4a857cbc0426cacf3a91c93261b840b4d547
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-09-15 22:43:52 +1200

    Create data folder and add firebase configurations

---
+++ busy_bee/src/data/service-agents/firebaseConfigs.js
@@ -0,0 +1,31 @@
+/**
+ * This file contains the configurations required to connect to the external database hosted on Firebase.
+ * Reference: https://docs.expo.dev/guides/using-firebase/
+ */
+import * as React from "react";
+
+import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Firebase configurations
+const firebaseConfig = {
+  apiKey: "AIzaSyDtIjqwxv5ZSwxDgGKprvQHyGHk4pbBBqs",
+  authDomain: "busy-bee-database.firebaseapp.com",
+  projectId: "busy-bee-database",
+  storageBucket: "busy-bee-database.appspot.com",
+  messagingSenderId: "765069339368",
+  appId: "1:765069339368:web:cd2918486e9a2f8b87b75d",
+  measurementId: "G-KQ7TMHHHVK",
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const dbh = firebase.firestore();
+
+export default dbh;
+
+// const analytics = getAnalytics(app);
