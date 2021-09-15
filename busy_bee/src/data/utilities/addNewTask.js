8ca9a924fdf3e47aad87139ee07ebc95a96b5684
Bea <jaymejoan@ecs.vuw.ac.nz>
2021-09-16 00:45:18 +1200

    Update addNewTask() so new lists and tasks are correctly based off inputted values instead of hardcoded

--- busy_bee/src/data/utilities/addNewTask.js
+++ busy_bee/src/data/utilities/addNewTask.js
@@ -2,17 +2,18 @@
  * After a user has created a new task, the task and its details are saved in the external database.
  * Each task is a document that will be saved under the collection with the same name as the specified list name.
  * If no list name is specified, the task will be saved in a default collection called "allTasks".
+ * Reference: https://firebase.google.com/docs/firestore/manage-data/add-data#web-version-8
  */
 import dbh from "@data/service-agents/firebaseConfigs.js";
 import taskData from "@data/utilities/storeTaskData";
 
 function addNewTask() {
-  dbh.collection("list-test").doc("task-test").set({
-    taskName: taskData.taskName,
-    dueDate: taskData.dueDate,
-    details: taskData.details,
-    listName: taskData.listName,
-  });
+    dbh.collection(taskData.listName).doc(taskData.taskName).set({
+      taskName: taskData.taskName,
+      dueDate: taskData.dueDate,
+      details: taskData.details,
+      listName: taskData.listName,
+    });
   printNewTask();
 }
 
@@ -25,3 +26,20 @@ function printNewTask() {
 }
 
 export default addNewTask;
+
+// dbh
+//     .collection("reports")
+//     .add({
+//       date: null,
+//       region: null,
+//       areaOfInjury: null,
+//       severity: null,
+//       equipmentInvolved: null,
+//     })
+//     .then((docRef) => {
+//       console.log("Document written with ID: ", docRef.id);
+//       reportID = docRef.id;
+//     })
+//     .catch((error) => {
+//       console.error("Error adding document: ", error);
+//     });
